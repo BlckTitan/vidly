74d3f0830bd55377b91300bc84e90e0eeb4fa999
BlckTitan <dorshman406@gmail.com>
2023-12-06 13:21:55 +0100

    Installed jest and wrote test for JWT generator function

---
+++ tests/unit/models/user.test.js
@@ -0,0 +1,16 @@
+const USER_MODEL = require('../../../model/userModel')
+const jwt = require('jsonwebtoken')
+const MONGOOSE = require('mongoose')
+
+describe('user.generateAuthToken', () => {
+    it('should return a valid JWT', () => {
+        const PAYLOAD = {
+            _id: new MONGOOSE.Types.ObjectId().toHexString, 
+            isAdmin: true
+        } 
+        const USER = new USER_MODEL(PAYLOAD)
+        const TOKEN = USER.generateAuthToken()
+        const DECODED = jwt.verify(TOKEN, process.env.JWT_PRIVATE_KEY)
+        expect(DECODED).toMatchObject(PAYLOAD)
+    })
+})
