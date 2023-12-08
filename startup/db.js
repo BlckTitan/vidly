08af5167645fc0d5f26bb124ccdda66ccd41c9f0
BlckTitan <dorshman406@gmail.com>
2023-12-08 21:23:47 +0100

    fixed a bug in the genre query integration test

--- startup/db.js
+++ startup/db.js
@@ -5,8 +5,9 @@ require('winston-mongodb')
 
 module.exports = function() {
     const DB = process.env.db;
+    const DB_TEST = process.env.db_test;
     //connecting to database
-    mongoose.connect(`${DB}`)
-    .then(() => WINSTON.info(`Connected to ${DB}...`))
+    mongoose.connect(`${DB_TEST}`)
+    .then(() => WINSTON.info(`Connected to ${DB_TEST}...`))
     // .catch(err => console.error('Could not connect to MongoDB...', err));
 }
--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,15 +1,32 @@
-const request = require('supertest')
+const request = require('supertest');
+const GENRE_MODEL = require('../../model/genreModel');
 
 let server;
 
 describe('/api/genre', () => {
     beforeEach(() => { server = require('../../index')})
-    afterEach(() => {server.close()})
+    afterEach( async () => {
+
+        await GENRE_MODEL.deleteMany()
+        server.close();
+       
+    })
 
     describe('GET/', () => {
         it('should return all genres', async () => {
-            const res = await request(server).get('/api/genre')
+
+            await GENRE_MODEL.collection.insertMany([
+                { name: 'genre1' },
+                { name: 'genre2' }
+            ])
+
+            const res = await request(server).get('/api/genre');
+
             expect(res.status).toBe(200)
-        })
-    })
-})
+            expect(res.body.length).toBe(2)
+            expect(res.body.some(g => g.name === 'genre1')).toBeTruthy()
+            expect(res.body.some(g => g.name === 'genre2')).toBeTruthy()
+
+        });
+    });
+});
